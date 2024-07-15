9607fb9e330438fa26dc51cfc576aa5f97bf0c13
Tomi Helin <tomi.helin@student.lut.fi>
2024-07-15 20:35:35 +0300

    Task 1 maybe

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require("mongoose")
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +16,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+const mongoDB = "mongodb://localhost:27017/templatedemo";
+mongoose.connect(mongoDB);
+mongoose.Promise = Promise;
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error"));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,7 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/user', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const {body, validationResult } = require("express-validator");
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const validateToken = require("../auth/validateToken.js")
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/list', validateToken, (req, res, next) => {
+  User.find({}, (err, users) =>{
+    if(err) return next(err);
+    res.send("User found");
+  })
+
+  res.send("Users not found.");
+});
+
+router.post('/register', 
+  body("email").isLength({min: 3}).trim().escape(),
+  body("password").isLength({min: 5}),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      return res.status(400).json({errors: errors.array()});
+    }
+    User.findOne({email: req.body.email}, (err, user) => {
+      if (err) {
+        console.log(err);
+        throw err
+      };
+      if (user) {
+        return res.status(403).json({email: "Email already in use."});
+      } else {
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(req.body.password, salt, (err, hash) => {
+            if(err) throw err;
+            User.create(
+              {
+                email: req.body.email,
+                password: hash
+              },
+              (err, ok) => {
+                if(err) throw err;
+                return res.redirect("/users/login");
+              }
+            );
+          });
+        });
+      }
+    });
 });
 
 module.exports = router;
