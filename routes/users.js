1ba571596052a83a3392ee3170d02dff50983249
Tomi Helin <tomi.helin@student.lut.fi>
2024-07-16 16:01:17 +0300

    Some fixes

--- routes/users.js
+++ routes/users.js
@@ -7,20 +7,50 @@ const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const validateToken = require("../auth/validateToken.js")
 
-router.get('/list', validateToken, (req, res, next) => {
-  User.find({}, (err, users) =>{
-    if(err) return next(err);
-    res.send("User found");
-  })
-
-  res.send("Users not found.");
+router.get('/private', validateToken, (req, res, next) => {
+  res.json({ email: req.user.email });
 });
 
-router.post('/login', function(req, res, next) {
-  res.send('test');
+router.post('/user/login', 
+  body("email").trim().escape(),
+  body("password").escape(),
+  async (req, res, next) => {
+
+    try {
+      const user = await User.findOne({email: req.body.email});
+      if (!user) {
+        return res.status(403).json({ error: "Invalid user or password."});
+      }
+
+      console.log(user);
+
+      bcrypt.compare(req.body.password, user.password, (err, isMatch) => {
+        if(err) throw err;
+        if(isMatch) {
+          const jwtPayload = {
+            id: user._id,
+            email: user.email
+          }
+          jwt.sign(
+            jwtPayload,
+            process.env.SECRET,
+            {
+              expiresIn: 120
+            },
+            (err, token) => {
+              res.json({success: true, token});
+            }
+          );
+        }
+      });
+    }
+    catch (err) {
+      console.log(err);
+      return res.status(500).json({ error: "Server error"});
+    }
 });
 
-router.post('/register', 
+router.post('/user/register', 
   body("email").isLength({min: 3}).trim().escape(),
   body("password").isLength({min: 5}),
   async (req, res, next) => {
